c7061fc9f7f8fbbd6da3684cc7e6186b96b3bf36
G-Sohal <gnrsohal@gmail.com>
2026-03-31 04:42:02 +0000

    soda pop

--- C1GamesStarterKit-master/python-algo/algo_strategy.py
+++ C1GamesStarterKit-master/python-algo/algo_strategy.py
@@ -5,7 +5,6 @@ import warnings
 from sys import maxsize
 import json
 
-"FREE"
 class AlgoStrategy(gamelib.AlgoCore):
     def __init__(self):
         super().__init__()
@@ -14,6 +13,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         gamelib.debug_write('Random seed: {}'.format(seed))
 
     def on_game_start(self, config):
+        """ Read in config and perform any initial setup here """
         gamelib.debug_write('Configuring your custom algo strategy...')
         self.config = config
         global WALL, SUPPORT, TURRET, SCOUT, DEMOLISHER, INTERCEPTOR, MP, SP
@@ -25,139 +25,206 @@ class AlgoStrategy(gamelib.AlgoCore):
         INTERCEPTOR = config["unitInformation"][5]["shorthand"]
         MP = 1
         SP = 0
+        self.scored_on_locations = []
+        
+        
+        self.loss_heatmap = {} 
+        self.current_structures = {} 
+        self.INTERCEPT_THRESHOLD = 3 
 
     def on_turn(self, turn_state):
+        """ This function is called every turn with the game state wrapper """
         game_state = gamelib.GameState(self.config, turn_state)
         game_state.suppress_warnings(True)
         self.starter_strategy(game_state)
         game_state.submit_turn()
 
     def starter_strategy(self, game_state):
-        """ The Dynamic Engine: Adaptive Battery, Sieve Expansion, and Path-Scored Swarms """
-        self.build_dynamic_battery(game_state)
-        self.build_dynamic_sieve(game_state)
+        """ The Smart Aegis Strategy: Upgraded Sieve with Reactive Interceptor """
+        self.build_upgraded_sieve(game_state)
+        self.build_reactive_defense(game_state)
+        self.heatmap_interceptor_defense(game_state)
         self.dynamic_shielded_offense(game_state)
 
-    def build_dynamic_battery(self, game_state):
+    def build_upgraded_sieve(self, game_state):
         """
-        Dynamically maintains a cluster of Supports in a designated 'safe zone'.
-        It checks if they exist, and builds/upgrades them if SP allows.
+        Builds and updates a defensive sieve based on the annotated plan.
+        Turn 1: Drops base setup with upgraded supports (38 SP total).
+        Subsequent turns: Prioritizes wall upgrades, then adds turrets.
         """
         
-        battery_zone = [[7, 11], [6, 11], [7, 10]]
+        anchors = [1, 9, 18, 26]
+        turrets_front = [3, 7, 11, 14, 16, 20, 24]
+        supports = [[12, 12], [15, 12]]
         
-        for loc in battery_zone:
+        
+        if game_state.turn_number == 0:
             
-            unit = game_state.contains_stationary_unit(loc)
-            if not unit:
-                if game_state.get_resource(SP) >= 4:
-                    game_state.attempt_spawn(SUPPORT, [loc])
+            game_state.attempt_spawn(SUPPORT, supports)
             
+            game_state.attempt_upgrade(supports)
             
-            unit = game_state.contains_stationary_unit(loc)
-            if unit and unit.unit_type == SUPPORT and not unit.upgraded:
-                if game_state.get_resource(SP) >= 4:
-                    game_state.attempt_upgrade([loc])
+            turret_coords = [[x, 13] for x in anchors]
+            game_state.attempt_spawn(TURRET, turret_coords)
+            
+            wall_coords = [[x, 13] for x in turrets_front]
+            game_state.attempt_spawn(WALL, wall_coords)
+        
+        
+        else:
+            
+            game_state.attempt_spawn(SUPPORT, supports)
+            game_state.attempt_upgrade(supports)
 
-    def build_dynamic_sieve(self, game_state):
-        """
-        Generates the Turret arc mathematically. Starts from the center 
-        and expands outward, skipping tiles to create a breathable sieve.
-        """
-        center_x = 13
-        
-        
-        left_xs = list(range(center_x - 1, -1, -3))  
-        right_xs = list(range(center_x + 2, 28, 3))  
-        
-        target_frontline_xs = []
-        for l, r in zip(left_xs, right_xs):
-            target_frontline_xs.extend([l, r])
             
+            turret_coords = [[x, 13] for x in anchors]
+            game_state.attempt_spawn(TURRET, turret_coords)
+            
+            
+            wall_coords = [[x, 13] for x in turrets_front]
+            game_state.attempt_spawn(WALL, wall_coords)
+
+            
+            
+            
+            for x in turrets_front:
+                loc = [x, 13]
+                unit = game_state.contains_stationary_unit(loc)
+                if unit and unit.unit_type == WALL and not unit.upgraded:
+                    if game_state.get_resource(SP) >= 1:
+                        game_state.attempt_upgrade([loc])
+
+            
+            turrets_back = [[4, 12], [8, 12], [19, 12], [23, 12]]
+            if game_state.get_resource(SP) >= 3:
+                game_state.attempt_spawn(TURRET, turrets_back)
+
+    def heatmap_interceptor_defense(self, game_state):
+        """
+        Tracks structure losses to build a heatmap and deploys an interceptor to the "hottest" nearby region.
+        """
         
-        for x in target_frontline_xs:
-            loc = [x, 13]
-            if not game_state.contains_stationary_unit(loc) and game_state.get_resource(SP) >= 3:
-                game_state.attempt_spawn(TURRET, [loc])
+        new_structures = {}
+        for x in range(28):
+            for y in range(14):
+                loc = [x, y]
+                unit = game_state.contains_stationary_unit(loc)
+                if unit:
+                    new_structures[(x, y)] = unit
 
         
-        left_xs_back = list(range(center_x - 2, -1, -3))
-        right_xs_back = list(range(center_x + 3, 28, 3))
-        
-        target_backline_xs = []
-        for l, r in zip(left_xs_back, right_xs_back):
-            target_backline_xs.extend([l, r])
+        for loc, unit in self.current_structures.items():
+            if loc not in new_structures:
+                x, y = loc
+                
+                
+                for nearby_x in range(x - 2, x + 3):
+                    if 0 <= nearby_x < 28:
+                        self.loss_heatmap[nearby_x] = self.loss_heatmap.get(nearby_x, 0) + 1
 
-        for x in target_backline_xs:
-            loc = [x, 12]
-            
-            if loc in [[7, 11], [6, 11], [7, 10], [6, 12], [7, 12]]: 
-                continue
-            if not game_state.contains_stationary_unit(loc) and game_state.get_resource(SP) >= 3:
-                game_state.attempt_spawn(TURRET, [loc])
+        
+        self.current_structures = new_structures
+        for x in self.loss_heatmap:
+            self.loss_heatmap[x] = max(0, self.loss_heatmap[x] - 0.25) 
+
+        
+        
+        if game_state.get_resource(MP) >= 3:
+            try:
+                
+                edges = game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_LEFT) + \
+                        game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
+                deploy_options = self.filter_blocked_locations(edges, game_state)
+                
+                
+                for edge in deploy_options:
+                    x = edge[0]
+                    nearby_loss_count = sum(self.loss_heatmap.get(nx, 0) for nx in range(x - 2, x + 3))
+                    if nearby_loss_count >= self.INTERCEPT_THRESHOLD:
+                        game_state.attempt_spawn(INTERCEPTOR, edge, 1)
+                        
+                        for nx in range(x - 2, x + 3):
+                            self.loss_heatmap[nx] = 0
+                        break 
+            except Exception as e:
+                gamelib.debug_write(f"Heatmap Interceptor error: {e}")
 
     def dynamic_shielded_offense(self, game_state):
         """
-        The Path-Scoring Engine: Simulates every valid edge, finds the path 
-        that intersects the highest number of friendly Support shields, and swarms it.
+        Simulates all paths and sends a massive pulse through the one with the highest shield score.
         """
         current_mp = game_state.get_resource(MP)
         
         
         if current_mp >= 8:
-            
-            my_supports = []
-            for x in range(28):
-                for y in range(14):
-                    loc = [x, y]
-                    unit = game_state.contains_stationary_unit(loc)
-                    if unit and unit.unit_type == SUPPORT:
-                        my_supports.append(unit)
+            try:
+                
+                edges = game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_LEFT) + \
+                        game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
+                valid_edges = self.filter_blocked_locations(edges, game_state)
+                
+                best_location = None
+                max_shield_score = -1
 
-            
-            edges = game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_LEFT) + \
-                    game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
-            valid_edges = self.filter_blocked_locations(edges, game_state)
-
-            best_spawn_loc = None
-            max_shield_score = -1
-
-            
-            if valid_edges:
-                for edge in valid_edges:
-                    path = game_state.find_path_to_edge(edge)
-                    shield_score = 0
-                    
-                    if path:
-                        for step in path:
-                            
-                            for support in my_supports:
+                if valid_edges:
+                    for location in valid_edges:
+                        path = game_state.find_path_to_edge(location)
+                        shield_score = 0
+                        
+                        if path:
+                            for path_location in path:
                                 
-                                dist = math.sqrt((step[0] - support.x)**2 + (step[1] - support.y)**2)
-                                
-                                if dist <= 8: 
-                                    shield_score += 1
+                                for support in [[12, 12], [15, 12]]:
                                     
-                    
-                    if shield_score > max_shield_score:
-                        max_shield_score = shield_score
-                        best_spawn_loc = edge
+                                    dist = math.sqrt((path_location[0] - support[0])**2 + (path_location[1] - support[1])**2)
+                                    if dist <= 12: 
+                                        shield_score += 1
+                                        
+                        
+                        if shield_score > max_shield_score:
+                            max_shield_score = shield_score
+                            best_location = location
+                
+                
+                if not best_location:
+                    best_location = [13, 0] 
 
                 
-                if not best_spawn_loc:
-                    best_spawn_loc = valid_edges[0]
+                game_state.attempt_spawn(SCOUT, best_location, 1000)
+            except Exception as e:
+                gamelib.debug_write(f"Dynamic offence simulation error: {e}")
 
-                
-                num_scouts = int(current_mp)
-                if num_scouts > 0:
-                    game_state.attempt_spawn(SCOUT, best_spawn_loc, num_scouts)
+    def build_reactive_defense(self, game_state):
+        """ Build upgraded walls to plug holes if scored upon. """
+        for location in self.scored_on_locations:
+            build_location = [location[0], location[1]+1]
+            if not game_state.contains_stationary_unit(build_location):
+                if game_state.get_resource(SP) >= 3:
+                    game_state.attempt_spawn(WALL, build_location)
+                    game_state.attempt_upgrade(build_location)
 
     def filter_blocked_locations(self, locations, game_state):
-        filtered = []
-        for location in locations:
-            if not game_state.contains_stationary_unit(location):
-                filtered.append(location)
-        return filtered
+        return [loc for loc in locations if not game_state.contains_stationary_unit(loc)]
+
+    def least_damage_spawn_location(self, game_state, location_options):
+        
+        damages = []
+        for location in location_options:
+            path = game_state.find_path_to_edge(location)
+            damage = 0
+            if path:
+                for path_location in path:
+                    damage += len(game_state.get_attackers(path_location, 0)) * 6
+            damages.append(damage if path else float('inf'))
+        return location_options[damages.index(min(damages))]
+
+    def on_action_frame(self, turn_string):
+        """ Action frame processor to track opponent breaches. """
+        state = json.loads(turn_string)
+        events = state.get("events", {})
+        for breach in events.get("breach", []):
+            if breach[4] != 1: 
+                self.scored_on_locations.append(breach[0])
 
 if __name__ == "__main__":
     algo = AlgoStrategy()
